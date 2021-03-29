483a79191d21e9f3356eab272c115c755a67df1f
Nik Charlebois <nicharl@microsoft.com>
2021-03-29 14:55:03 -0400

    Updates to populate data

--- ARM Templates/PopulateCosmosDB.ps1
+++ ARM Templates/PopulateCosmosDB.ps1
@@ -1,3 +1,16 @@
+param(
+    [string]
+    $MasterKey,
+
+    [string]
+    $EndpointURI,
+
+    [string]
+    $DatabaseID,
+
+    [string]
+    $ContainerID
+)
 Add-Type -AssemblyName System.Web
 
 Function Generate-MasterKeyAuthorizationSignature{
@@ -26,26 +39,29 @@ Function Generate-MasterKeyAuthorizationSignature{
     return $key
 }
 Write-Host "Here"
-$endpoint = "https://sql-cosmos.documents.azure.com:443/"
-$MasterKey = "e5yd9mTesLUOLa6fBRZKM5wHhrkPv7paXtFjzzP5u6tFsmdsq3MD6TNLuoij74fMyU0uBjcQGNt09jgi1VS6RQ=="
 
 $KeyType = "master"
 $TokenVersion = "1.0"
 $date = Get-Date
 $utcDate = $date.ToUniversalTime()
 $xDate = $utcDate.ToString('r', [System.Globalization.CultureInfo]::InvariantCulture)
-$databaseId = "mgcstorelbdj2je"
-$containerId = "mgccontainer"
 $itemId = "TestItem"
 $itemResourceType = "docs"
-$itemResourceId = "dbs/"+$databaseId+"/colls/"+$containerId
+$itemResourceId = "dbs/" + ${Env:DatabaseID} + "/colls/" + ${Env:ContainerID}
 
-$itemResourceLink = "dbs/"+$databaseId+"/colls/"+$containerId+"/docs"
+$itemResourceLink = "dbs/" + ${Env:DatabaseID} + "/colls/" + ${Env:ContainerID} + "/docs"
 $verbMethod = "POST"
 
-$requestUri = "$endpoint$itemResourceLink"
+$requestUri = "${Env:EndpointURI}$itemResourceLink"
 
-$authKey = Generate-MasterKeyAuthorizationSignature -Verb $verbMethod -ResourceId $itemResourceId -ResourceType $itemResourceType -Date $xDate -MasterKey $MasterKey -KeyType $KeyType -TokenVersion $TokenVersion
+$authKey = Generate-MasterKeyAuthorizationSignature -Verb $verbMethod `
+    -ResourceId $itemResourceId `
+    -ResourceType $itemResourceType `
+    -Date $xDate `
+    -MasterKey ${Env:MasterKey} `
+    -KeyType $KeyType `
+    -TokenVersion $TokenVersion
+
 Write-Host "AuthKey:$authKey"
 $header = @{
     "authorization"         = "$authKey";
@@ -54,14 +70,38 @@ $header = @{
     "x-ms-date"             = "$xDate";
     "Accept"                = "application/json";
     "User-Agent"            = "PowerShell-RestApi-Samples";
-    "x-ms-documentdb-partitionkey" = '["testPk"]'
+    "x-ms-documentdb-partitionkey" = '["ASUS Zenbook Jeeze"]'
 }
 
 $ItemDefinition = @"
 {
-    "id": "$itemId",
-    "testProperty": "test",
-    "pk": "testPk"
+    "id": "259bb32d90d648598fd372b4b3c0393e",
+    "title": "ASUS Zenbook Jeeze",
+    "thumbnailUrl": "https://img-prod-cms-rt-microsoft-com.akamaized.net/cms/api/am/imageFileData/RE3zid7?ver=37e7&q=90&m=6&h=270&w=270&b=%23FFFFFFFF&f=jpg&o=f&aim=true",
+    "company": "Asus",
+    "category": "Laptop",
+    "description": "Pro VInny Slim and stylish, available in 13.5 inch and new 15 inch touchscreens, rich color options, and two durable finishes. Make a powerful statement and get improved speed, performance, and all-day battery life.",
+    "price": 100.11,
+    "url": "https://www.microsoft.com/en-us/p/surface-laptop-3/8vfggh1r94tm?activetab=overview&cid=msft_web_collection",
+    "audience": [
+        {
+            "id": "e09e4dce-4ec1-4f9e-a482-107eec83a376",
+            "type": "user"
+        },
+        {
+            "id": "267eb93c-c6c4-4f4e-b6f1-5671103eb8ee",
+            "type": "user"
+        },
+        {
+            "id": "5e2ff59c-bc04-4bbe-b046-03acd420b71a",
+            "type": "user"
+        }
+    ],
+    "_rid": "ta5nAKDymXwDAAAAAAAAAA==",
+    "_self": "dbs/ta5nAA==/colls/ta5nAKDymXw=/docs/ta5nAKDymXwDAAAAAAAAAA==/",
+    "_etag": "\"55003292-0000-0200-0000-605e1a900000\"",
+    "_attachments": "attachments/",
+    "_ts": 1616779920
 }
 "@
 
